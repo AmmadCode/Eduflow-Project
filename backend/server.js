fb85cfe1cc5c11f47c9537f092eb8919f46e681e
Muhammad Ammad <kingworkgroup@gmail.com>
2026-03-24 19:33:14 +0500

    fix error

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,22 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://eduflow-project-murex.vercel.app",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // 🔥 Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 const port = process.env.PORT || 3000;
 
