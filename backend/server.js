3af29118de7f3f5527d3997c78f5b832bbe24ec9
Muhammad Ammad <kingworkgroup@gmail.com>
2026-03-24 19:39:19 +0500

    fix error

--- backend/server.js
+++ backend/server.js
@@ -3,42 +3,31 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
-import authRouter from "./routes/authRoutes.js";
-import courseRouter from "./routes/courseRoutes.js";
-import enrollRouter from "./routes/enrollmentRoutes.js";
-import adminRouter from "./routes/adminRoutes.js";
-import userRouter from "./routes/userRoutes.js";
+
 const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://eduflow-project-murex.vercel.app",
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  // 🔥 Handle preflight request
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+app.use(
+  cors({
+    origin: "https://eduflow-project-murex.vercel.app",
+    credentials: true,
+  }),
+);
 
-  next();
-});
+app.options("*", cors());
 
-const port = process.env.PORT || 3000;
-
-// api endpoints
+// routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/courses", courseRouter);
 app.use("/api/enroll", enrollRouter);
-app.use("/api/admin", adminRouter); // admin routes
+app.use("/api/admin", adminRouter);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on Port ${port}`);
+  console.log(`Server running on ${port}`);
 });
